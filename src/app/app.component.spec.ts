b4d0c64be99b9182724fbbdfe3b509d4895e6072
sdulal123 <sushildulal123@gmail.com>
2024-05-04 10:25:44 +0200

    00_ChessMe-build "finished"

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -2,13 +2,9 @@ import { TestBed } from '@angular/core/testing';
 import { AppComponent } from './app.component';
 
 describe('AppComponent', () => {
-  beforeEach(async () => {
-    await TestBed.configureTestingModule({
-      declarations: [
-        AppComponent
-      ],
-    }).compileComponents();
-  });
+  beforeEach(() => TestBed.configureTestingModule({
+    declarations: [AppComponent]
+  }));
 
   it('should create the app', () => {
     const fixture = TestBed.createComponent(AppComponent);
@@ -16,16 +12,16 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have as title 'chessMe'`, () => {
+  it(`should have as title 'chess-game'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
-    expect(app.title).toEqual('chessMe');
+    expect(app.title).toEqual('chess-game');
   });
 
   it('should render title', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('.content span')?.textContent).toContain('chessMe app is running!');
+    expect(compiled.querySelector('.content span')?.textContent).toContain('chess-game app is running!');
   });
 });
--- src/app/chess-logic/pieces/piece.ts
+++ src/app/chess-logic/pieces/piece.ts
@@ -4,17 +4,17 @@ export abstract class Piece {
     protected abstract _FENChar: FENChar;
     protected abstract _directions: Coords[];
 
-    constructor (private _color: Color) {}
+    constructor(private _color: Color) { }
 
     public get FENChar(): FENChar {
         return this._FENChar;
     }
 
-    public get direction(): Coords[] {
+    public get directions(): Coords[] {
         return this._directions;
     }
 
-    public get color() : Color {
+    public get color(): Color {
         return this._color;
     }
 }
